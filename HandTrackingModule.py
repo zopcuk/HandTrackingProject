719299381b083dafdb1ae6ba99c79aa7bbf36d8e
zopcuk <f.zopck@gmail.com>
2022-08-31 17:20:17 +0300

    new feature for findPosition func

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -31,6 +31,9 @@ class handDetector():
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
         self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -38,11 +41,18 @@ class handDetector():
                 # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
                 # print(id, cx, cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
-        return self.lmList
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20), (0, 255, 0), 2)
+        return self.lmList, bbox
 
     def fingersUp(self):
         fingers = []
